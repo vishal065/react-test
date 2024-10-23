01365a68512ef5a0612e6855ebc1df046ae81fff
vishal <dasvishal60@gmail.com>
2024-10-23 23:44:50 +0530

    FIX

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://test-backend-nh9c.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
